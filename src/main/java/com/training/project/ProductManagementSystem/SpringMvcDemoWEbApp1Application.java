17d942e059aa5444dcd094fad4243db295597cce
Shivani <shivanigupta141999@gmail.com>
2020-08-09 19:05:26 +0530

    demo

---
+++ src/main/java/com/training/project/ProductManagementSystem/SpringMvcDemoWEbApp1Application.java
@@ -0,0 +1,13 @@
+package com.training.project.ProductManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcDemoWEbApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcDemoWEbApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/project/ProductManagementSystem/SpringMvcDemoWEbApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.project.ProductManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcDemoWEbApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
